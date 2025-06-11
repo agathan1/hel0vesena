97bdfff57294538ce25912c1af2b4c31b3ebf281
agathan1 <agathantahtawia@gmail.com>
2025-06-12 01:47:00 +0700

    🐛 : Last try fixing the height for confirm

--- src/component/templates/SplashScreen.tsx
+++ src/component/templates/SplashScreen.tsx
@@ -2,7 +2,7 @@ import Logo from "@/assets/logo/logo.png";
 
 export default function SplashScreen() {
   return (
-    <main className=" h-screen flex flex-col text-center justify-around px-5">
+    <main className=" h-[100dvh] flex flex-col text-center justify-around px-5">
       <div className="m-auto">
         <img src={Logo} alt="Logo Bisnis" className="size-88 mx-auto " />
       </div>
